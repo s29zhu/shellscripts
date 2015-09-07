e04ed6a3aa7f276cfb653ea76df5e8ffb15440ae
s29zhu <shashazhu1989@gmail.com>
2015-09-07 00:52:57 -0600

    more

--- IOStreamandRedirection.sh
+++ IOStreamandRedirection.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# direct good result to File and error code to ErrorFile
-printf "%s%v" OK Error >> File 2> ErrorFile
-# direct good result to File and discard the error message 
-printf "%s%v" OK Error >> File 2> /dev/null
-printf "%s\n" OK >> File
+# direct standard output to File and standard error code to ErrorFile
+#printf "%s%v" OK Error > File 2> ErrorFile
+# direct standard output to File and discard the standard error 
+#printf "%s%v" OK Error > File 2>/dev/null
+# direct fine result and Error to the standard output stream which is
+# redirected to File before, so the error will go to File as well
+printf "%s%v\n" OK Error > File 2>&1
+printf "%s\n%v" OK Error > File 2>&1
+printf "%s%v"  OK Error 2>&1 > File
+#exec command to redirect to the I/O streams for the rest of the script
+exec 1>standoutFile
+exec 0<inputFile
+exec 2>errorFile
